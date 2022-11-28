ac00aae12d79e6ff4eb2d21ffb543c5056935090
suryaking614 <109645567+suryaking614@users.noreply.github.com>
2022-11-28 22:19:58 +0530

    codee

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+
+
+const Welcome=({ name }) =>{
+    return(
+        <>
+            <h1>Hey{name}</h1>
+            <h2>welcome to newton school</h2>
+        </>
+    )
+}
+export default Welcome;
