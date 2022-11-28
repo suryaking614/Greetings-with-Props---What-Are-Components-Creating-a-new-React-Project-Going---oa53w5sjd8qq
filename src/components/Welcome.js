31791c0f664a61e4ff241e285cf6d994ea05ed18
suryaking614 <109645567+suryaking614@users.noreply.github.com>
2022-11-29 00:46:31 +0530

    codes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import React from "react";
 const Welcome=({ name }) =>{
     return(
         <>
-            <h1>Hey{name}</h1>
+            <h1>Hey {name}!</h1>
             <h2>welcome to newton school</h2>
         </>
     )
