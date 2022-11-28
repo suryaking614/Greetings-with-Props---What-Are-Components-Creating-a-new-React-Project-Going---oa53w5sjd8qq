74ab363429cd686688f3a4fffe20a9c4dfae8e53
suryaking614 <109645567+suryaking614@users.noreply.github.com>
2022-11-29 00:53:14 +0530

    code

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ const Welcome=({ name }) =>{
     return(
         <>
             <h1>Hey {name}!</h1>
-            <h2>welcome to newton school</h2>
+            <h2>Welcome to Newton School</h2>
         </>
     )
 }
